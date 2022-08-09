870445b8b72ee4a29945537672d3e0e278b1c9e3
felixaldam-gates <44896994+Felixim0@users.noreply.github.com>
2022-08-09 16:44:22 +0100

    Added gpio code

--- api/deviceHelpers.js
+++ api/deviceHelpers.js
@@ -1,5 +1,22 @@
+import * as gpiop from 'rpi-gpio';
 
 export async function listAll(req) {
+  gpiop.setup(7, gpiop.DIR_OUT)
+    .then(() => {
+        return gpiop.write(7, true)
+    })
+    .catch((err) => {
+        console.log('Error: ', err.toString())
+    })
+  await sleep(4000);
+  gpiop.setup(7, gpiop.DIR_OUT)
+    .then(() => {
+        return gpiop.write(7, false)
+    })
+    .catch((err) => {
+        console.log('Error: ', err.toString())
+    })
+
   return 'Not setup yet'
 }
 
